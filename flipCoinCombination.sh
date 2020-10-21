a0352ecad90efb4ef443d78805f8cd1593afa9db
“anurag0401” <xoom.aj@gmail.com>
2020-10-21 13:47:59 +0530

    Addeda code to store the singlet combination in a dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -12,3 +12,35 @@ then
 else
 	echo "Tails win"
 fi
+
+declare -A coin
+
+coin[heads]=0
+coin[tails]=0
+a=0
+
+Single (){
+	if [ $1 -eq 0 ]
+	then
+		echo "Heads"
+		((coin[heads]++))
+	else
+		echo "Tails"
+		((coin[tails]++))
+	fi
+}
+
+while [ $a -le 10 ]
+do
+	FlipCoin=$(( $RANDOM%2 ))
+	Single $FlipCoin
+	((a++))
+done
+echo "no. of times it was Head:" ${coin[heads]}
+echo "no. of times it was Tail:" ${coin[tails]}
+
+PofHeads=$(( (${coin[heads]}*100)/10 ))
+PofTails=$(( (${coin[tails]}*100)/10 ))
+
+echo "percentage of heads:" $PofHeads
+echo "percentage of Tails:" $PofTails
